0a1878bac49f4a42b9d2cf42a25a5c723d73ed10
= <daniell.felipe.2017@gmail.com>
2025-03-16 14:59:30 -0300

    make cards functional

--- src/pages/app/dashboard/month-canceled-orders-amount.tsx
+++ src/pages/app/dashboard/month-canceled-orders-amount.tsx
@@ -1,7 +1,14 @@
+import { getMonthCanceledOrdersAmount } from '@/api/get-month-canceled-amount'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { useQuery } from '@tanstack/react-query'
 import { DollarSign } from 'lucide-react'
 
 export function MonthCanceledOrdersAmount() {
+  const { data: monthCanceledOrdersAmount } = useQuery({
+    queryFn: getMonthCanceledOrdersAmount,
+    queryKey: ['metrics', 'month-canceled-orders-amount'],
+  })
+
   return (
     <Card className="gap-2">
       <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
@@ -11,11 +18,25 @@ export function MonthCanceledOrdersAmount() {
         <DollarSign className="size-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        <span className="font-bold text-2xl tracking-tight">32</span>
-        <p className="text-muted-foreground text-xs">
-          <span className="text-emerald dark:text-emerald-400">-2%</span> em
-          relação a otem
-        </p>
+        {monthCanceledOrdersAmount && (
+          <>
+            <span className="font-bold text-2xl tracking-tight">
+              {monthCanceledOrdersAmount.amount.toLocaleString('pt-BR')}
+            </span>
+            <p className="text-muted-foreground text-xs">
+              {monthCanceledOrdersAmount.diffFromLastMonth < 0 ? (
+                <span className="text-emerald dark:text-emerald-400">
+                  {monthCanceledOrdersAmount.diffFromLastMonth}
+                </span>
+              ) : (
+                <span className="text-rose dark:text-rose-400">
+                  +{monthCanceledOrdersAmount.diffFromLastMonth}
+                </span>
+              )}{' '}
+              em relação ao mês passado
+            </p>
+          </>
+        )}
       </CardContent>
     </Card>
   )
